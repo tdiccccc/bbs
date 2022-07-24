d35e54d4302ab36e1fe7fbc5e46a5f8fa7742ccf
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-07-24 21:48:16 +0900

    [Add]

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BBS</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1 class="title">PHP</h1>
+  <form action="" class="formWrapper">
+    <div class="boardWrapper">
+      <div>
+        <input type="submit" value="書き込む">
+        <label for="">名前:</label>
+        <input type="text" name="username">
+      </div>
+      <div>
+        <textarea class="commentTextArea"></textarea>
+      </div>
+    </div>
+  </form>
+</body>
+</html>
