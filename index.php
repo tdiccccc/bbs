59f7a9240189b0941662de0f5aaa7e23be0a3833
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-07-24 23:34:41 +0900

    [fix]

--- index.php
+++ index.php
@@ -1,16 +1,46 @@
 <?php
-  if (!empty($_POST["submitButton"])){//empty関数＝値が空かどうかの判定。!で反転させる。
-    echo $_POST["username"];//$_POSTなどはスーパーグローバル変数という
-    echo $_POST["comment"];
+
+  $comment_array = array();
+  $db = null;
+  
+  
+  //データベース接続
+  try {
+    $db = new PDO('mysql:host=localhost;dbname=bbs','root','root');
+  } catch(PDOException $e) {
+    echo $e->getMessage();
   }
 
-  //データベース接続
-  $db = new PDO('mysql:host=localhost,dbname=bbs','root','root');
-?>
+  //フォームを打ち込んだ時
+  if (!empty($_POST["submitButton"])){//empty関数＝値が空かどうかの判定。!で反転させる。
+    
+    $postDate = date("Y-m-d H:i:s");
+    
+    try {
+      //$_POSTなどはスーパーグローバル変数という
+      $stmt = $db->prepare("INSERT INTO `bbs-table` (`name`, `comment`, `postDate`) VALUES (:name, :comment, :postDate);");
+      $stmt->bindParam(':name', $_POST["username"], PDO::PARAM_STR);
+      $stmt->bindParam(':comment', $_POST["comment"], PDO::PARAM_STR);
+      $stmt->bindParam(':postDate', $postDate, PDO::PARAM_STR);
+      
+      $stmt->execute();
+    } catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+  }
+  
+  
+  //DBからコメントデータを取得する
+  $sql = "SELECT * FROM `bbs-table`;";
+  $comment_array = $db->query($sql);
+  
+  //DBの接続を閉じる
+  $db = null;
+  ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
+  <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,13 +52,18 @@
   <form action="" class="formWrapper" method="POST">
     <div class="boardWrapper">
       <section>
-        <article>
-          <div class="wrapper">
-            <span>名前:</span>
-            <p class="username"></p>
-            <time>2020/10/15</time>
-          </div>
-        </article>
+        <?php foreach($comment_array as $comment) : ?>
+          <article>
+            <div class="wrapper">
+              <div class="nameArea">
+                <span>名前:</span>
+                <p class="username"><?php echo $comment["name"]; ?></p>
+                <time><?php echo $comment["postDate"] ?></time>
+              </div>
+            </div>
+            <p class="comment"><?php echo $comment["comment"];?></p>
+          </article>
+        <?php endforeach; ?>
       </section>
       <div>
         <input type="submit" value="書き込む" name="submitButton">
